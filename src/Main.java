2babf4d9bb95e450118fe93a8271656363024d65
renat <renatogarcia1@hotmail.com>
2022-11-27 20:00:46 -0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+    public static void main(String[] args) {
+       Curso curso1 = new Curso();
+       curso1.setTitulo("Curso java");
+       curso1.setDescricao("descrição curso java");
+       curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso js");
+        curso2.setDescricao("descrição curso js");
+        curso2.setCargaHoraria(4);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("mentoria de java");
+        mentoria1.setDescricao("descrição mentoria java");
+       // mentoria.setData()
+
+    }
+}
